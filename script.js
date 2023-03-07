d5d5cd147c8a39c1286c37b920c13b1eb65db713
Shuffle10 <saphal.pant@westcliff.edu>
2023-03-07 19:09:46 +0545

    Everything all at once

---
+++ script.js
@@ -0,0 +1,20 @@
+function checkText(){
+    let text = document.getElementById("textBox").value;
+    let textValidate = /[\u0900-\u097F]/;
+
+    if (textValidate.test(text)){
+        document.getElementById("message").style.color = "green";
+        document.getElementById("message").innerHTML = "Well Done!";
+ 
+    }
+    else{
+        document.getElementById("message").style.color = "red";
+        document.getElementById("message").innerHTML = "Not in Nepali Language"   
+    }
+
+}
+
+
+
+
+// [\u0900-\u097F]
